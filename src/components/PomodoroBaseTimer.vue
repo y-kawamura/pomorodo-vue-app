06828350bd9dbb3633fcf5828e63ffd5fba05182
y-kawamura <tetora.y@gmail.com>
2020-05-08 21:15:29 +0900

    Change pomodoro timer design

--- src/components/PomodoroBaseTimer.vue
+++ src/components/PomodoroBaseTimer.vue
@@ -1,16 +1,72 @@
 <template>
-  <div>
-    <span class="time">{{ workTimerMin }} : {{ workTimerSec }}</span>
-    <!-- start -->
-    <button v-show="!autoStart" :disabled="!!workTimer" @click="startTimer">
-      Start
-    </button>
-    <!-- pause -->
-    <button v-show="!isPause" @click="pauseTimer">Pause</button>
-    <button v-show="isPause" @click="restartTimer">Restart</button>
-    <!-- stop -->
-    <button v-show="!skip" @click="cancelTimer">Cancel</button>
-    <button v-show="skip" @click="skipTimer">Skip</button>
+  <div clas="pomodoro-timer">
+    <div class="pomodoro-timer--time-group">
+      <span class="pomodoro-timer--time"
+        >{{ workTimerMin }} : {{ workTimerSec }}</span
+      >
+      <svg
+        class="pomodoro-timer--circle"
+        width="300"
+        height="300"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g>
+          <circle
+            id="circle"
+            class="circle-animation"
+            r="144"
+            cy="150"
+            cx="150"
+            stroke-width="8"
+            fill="none"
+            :stroke-dasharray="circleLength"
+            :stroke-dashoffset="circleLength - restCircle"
+            :stroke="circleColor"
+          />
+        </g>
+      </svg>
+    </div>
+    <div class="pomodoro-timer--button-group">
+      <!-- start -->
+      <button
+        v-show="!isWorking"
+        @click="startTimer"
+        class="pomodoro-timer--button pomodoro-timer--button-red"
+      >
+        Start
+      </button>
+      <!-- pause -->
+      <button
+        v-show="isWorking && !isPause"
+        @click="pauseTimer"
+        class="pomodoro-timer--button pomodoro-timer--button-orange"
+      >
+        Pause
+      </button>
+      <button
+        v-show="isWorking && isPause"
+        @click="restartTimer"
+        class="pomodoro-timer--button pomodoro-timer--button-red"
+      >
+        Restart
+      </button>
+      <!-- stop -->
+      <button
+        v-show="!skip"
+        @click="cancelTimer"
+        disabled
+        class="pomodoro-timer--button pomodoro-timer--button-gray"
+      >
+        Cancel
+      </button>
+      <button
+        v-show="skip"
+        @click="skipTimer"
+        class="pomodoro-timer--button pomodoro-timer--button-gray"
+      >
+        Skip
+      </button>
+    </div>
   </div>
 </template>
 
@@ -45,6 +101,20 @@ export default {
     workTimerSec() {
       return ('00' + (this.workTime % 60)).slice(-2)
     },
+    isWorking() {
+      return !!this.workTimer
+    },
+    circleLength() {
+      return 2 * 144 * Math.PI
+    },
+    restCircle() {
+      return this.isWorking
+        ? this.circleLength * ((this.workTime - 1) / this.settingTime)
+        : this.circleLength
+    },
+    circleColor() {
+      return !this.autoStart ? '#4d6fcc' : '#4dbb91'
+    },
   },
   methods: {
     countDown() {
@@ -52,9 +122,11 @@ export default {
         this.workTime -= 1
 
         if (this.workTime === 0) {
-          this.$emit('done', { startTime: this.startTime, endTime: new Date() })
+          this.$emit('done', {
+            startTime: this.startTime,
+            endTime: new Date(),
+          })
           clearInterval(this.workTimer)
-          this.workTime = this.settingTime
         }
       }, 1000)
     },
@@ -77,25 +149,92 @@ export default {
     skipTimer() {
       this.$emit('done', { startTime: this.startTime, endTime: new Date() })
       clearInterval(this.workTimer)
-      this.workTime = this.settingTime
-    },
-  },
-  watch: {
-    settingTime(newVal) {
-      this.workTime = newVal
     },
   },
   mounted() {
+    this.workTime = this.settingTime
+
+    // auto start after 2sec
     if (this.autoStart) {
-      this.startTimer()
+      setTimeout(() => {
+        this.startTimer()
+      }, 2000)
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.time {
-  display: block;
-  font-size: 5rem;
+.pomodoro-timer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &--time-group {
+    position: relative;
+    width: 300px;
+    height: 300px;
+    margin: 0 auto;
+  }
+
+  &--time {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 3rem;
+  }
+
+  &--circle {
+    transform: rotate(-90deg);
+
+    .circle-animation {
+      transition: 1000ms linear;
+    }
+  }
+
+  &--button-group {
+    margin: -10px auto 0;
+    width: 300px;
+    display: flex;
+    flex-flow: row-reverse;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &--button {
+    width: 80px;
+    height: 80px;
+    font-size: 1.2rem;
+    border-radius: 50%;
+    /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.6); */
+    outline: none;
+    cursor: pointer;
+    transition: 600ms ease-in-out;
+    /* box-shadow: none; */
+  }
+  &--button-red {
+    border: 1px solid #ff6a6a;
+    color: #ff6a6a;
+    &:hover {
+      background-color: #fcd4d4;
+      color: #d84d4d;
+    }
+  }
+  &--button-orange {
+    border: 1px solid #ffa033;
+    color: #ffa033;
+    &:hover {
+      background-color: #ffd6a8;
+      color: #df831a;
+    }
+  }
+  &--button-gray {
+    border: 1px solid #867e7e;
+    color: #867e7e;
+    &:hover {
+      background-color: #dbd4d4;
+      color: #726767;
+    }
+  }
 }
 </style>
